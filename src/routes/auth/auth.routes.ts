93bee646ceb17c10f6ed28f0bde7a308859c5438
Wrathful Cornsilk Jackrabbit <X7dqbY6a@proton.me>
2023-02-23 16:31:45 +0000

    #10 - Add missing comments and endpoint

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -3,16 +3,23 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
+    // TODO Implement
     res.send('Register');
 });
 
 router.get('/signin', (req, res) => {
+    // TODO Implement
     res.send('Login');
 });
 
 router.get('/password-reset', (req, res) => {
+    // TODO Implement
     res.send('Password Reset');
 });
 
+router.get('/2fa', (req, res) => {
+    // TODO Implement
+    res.send('2 Factor Authentication');
+});
 
 export default router;
