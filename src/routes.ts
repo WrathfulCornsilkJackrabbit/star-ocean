69675117dc76e0ae829e5556c6418a3122f0b12f
Wrathful Cornsilk Jackrabbit <X7dqbY6a@proton.me>
2023-02-23 16:31:24 +0000

    #10 - Use the Auth route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import auth from './routes/auth/auth.routes';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+router.use('/auth', auth);
 
 export default router;
